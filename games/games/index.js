cebb8370cffc83f3983298ab2d56d08127539740
Kardna <76856768+ZenfyDev@users.noreply.github.com>
2023-01-17 20:15:24 -0500

    kardna

--- games/games/index.js
+++ games/games/index.js
@@ -2,7 +2,7 @@
 //vars
 var backend = "aHR0cHM6Ly82NjlmLTY5LTI0OC0yMjctMTMxLm5ncm9rLmlvLw==";
 var url = atob(backend);
-var roblox = "aHR0cHM6Ly9reGQuZm0vcm9ibG94";
+var roblox = "aHR0cHM6Ly9reGQuZm0vcm9ibG94L3Yy";
 var krunker = "aHR0cHM6Ly9icm93c2VyZnBzLmNvbS8=";
 var BBstars = "aHR0cHM6Ly93d3ctaWctb3BlbnNvY2lhbC5nb29nbGV1c2VyY29udGVudC5jb20vZ2FkZ2V0cy9pZnI/dXJsPWh0dHBzOi8vc2l0ZXMuZ29vZ2xlLmNvbS9zaXRlL2Jhc2tldGJhbGxsZWdlbmR1bmJsb2NrZWQvYmFza2V0YmFsbF9zdGFycy54bWw=";
 var pk = "Li9nZmlsZXMvZ2ZpbGVzL2VtL3Bva2Vtb25zaWx2ZXIvaW5kZXguaHRtbA==";
@@ -14,7 +14,7 @@ var doomz = "aHR0cHM6Ly9kb29tei5pby8=";
 var stumble = "aHR0cHM6Ly9ub3cuZ2cvcGxheS9raXRrYS1nYW1lcy83OTk5L3N0dW1ibGUtZ3V5cy8=";
 var amongus = "aHR0cHM6Ly9ub3cuZ2cvcGxheS9pbm5lcnNsb3RoLWxsYy80MDQ3L2Ftb25nLXVz";
 var territorial = "aHR0cHM6Ly90ZXJyaXRvcmlhbC5pby8=";
-var google = "aHR0cHM6Ly9nb29nbGUuY29t";
+var google = "aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8=";
 
 //search page id = main-iframe-google-please-work
 document.getElementById('main-iframe-google-please-work'.src = url + "/uv.html#" + google);
