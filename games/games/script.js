831c642f2124fff3ced3ee85420a199b591f5014
Kardna <76856768+ZenfyDev@users.noreply.github.com>
2023-01-20 08:38:01 -0500

    Update script.js

--- games/games/script.js
+++ games/games/script.js
@@ -1,4 +1,4 @@
-var newsrc = url +"/uv.html#" + "ZmlsZTovLy9DOi9Vc2Vycy9KYWNrJTIwQ2xhbmN5L0RvY3VtZW50cy9HaXRIdWIvMy4xL0thcmRuYS9nYW1lcy9nYW1lcy9nZmlsZXMvZ2ZpbGVzL2VtL3Bva2Vtb25zaWx2ZXIvaW5kZXguaHRtbA==";
+var newsrc = url +"/uv.html#" + "Z29vZ2xlLmNvbQ==";
 setTimeout(() => 
 { 
   document.getElementById("main-iframe-google-please-work").src = newsrc;
