2a20d9eff515f9d2ce3df8b67908a001d6446330
Zenfy <jackclancy470@gmail.com>
2023-03-08 06:47:36 -0500

    v4

--- v4/games/files/cloud/app.js
+++ v4/games/files/cloud/app.js
@@ -2,24 +2,15 @@
 
 // By Zenfy //
 
-// Start Get Proxy Url //
-
-let site = getCurrentURL();
-var name = new URL(site).host;
-var url = 'bg' + '.' + name;
 console.log(url);
-function getCurrentURL () {
-return window.location.href
-}
-
-// End Get Proxy Url //
 
 // Start Of Games //
 
 function krunker(){
     var win = window.open('./plyr/')
     let temp_url = "https://krunker.io"
+    console.log(url);
     let temp_url2 = btoa(url);
-    var url =  url + "/uv.html#" + temp_url2
-    setTimeout(function(){var iframe = win.document.createElement('iframe'); iframe.style.position = "absolute"; iframe.style.top = "0"; iframe.style.bottom = "0"; iframe.style.left = "0"; iframe.style.right = "0"; iframe.style.width = "100%"; iframe.style.height = "100%"; iframe.style.border = "none"; iframe.style.margin = "none"; iframe.style.zIndex = "999999"; iframe.src = url; win.document.body.appendChild(iframe)}, 500); 
+    var url =  "https://krunker.io";
+    setTimeout(function(){var iframe = win.document.createElement('iframe'); iframe.style.position = "absolute"; iframe.style.top = "0"; iframe.style.bottom = "0"; iframe.style.left = "0"; iframe.style.right = "0"; iframe.style.width = "100%"; iframe.style.height = "100%"; iframe.style.border = "none"; iframe.style.margin = "none"; iframe.style.zIndex = "999999"; iframe.src = url; win.document.body.appendChild(iframe)}, 100); 
 }
