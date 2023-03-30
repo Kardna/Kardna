716bcf2ed2ba1b5903310c0d108b1afe93e7cd99
Zenfy <jackclancy470@gmail.com>
2023-03-30 15:58:06 -0400

    Update app.js

--- v4/games/files/cloud/app.js
+++ v4/games/files/cloud/app.js
@@ -8,18 +8,18 @@ console.log(url);
 
 function krunker(){
     let url2 =  "https://krunker.io";
-    var win = window.open('./plyr/')
-    var temp_url2 = btoa(url2);
+    let win = window.open('./plyr/')
+    let temp_url2 = btoa(url2);
     let src = "https://" + url + "/uv.html#" + temp_url2
-    setTimeout(function(){var iframe = win.document.createElement('iframe'); iframe.style.position = "absolute"; iframe.style.top = "0"; iframe.style.bottom = "0"; iframe.style.left = "0"; iframe.style.right = "0"; iframe.style.width = "100%"; iframe.style.height = "100%"; iframe.style.border = "none"; iframe.style.margin = "none"; iframe.style.zIndex = "999999"; iframe.src = src; win.document.body.appendChild(iframe)}, 100); 
+    setTimeout(function(){let iframe = win.document.createElement('iframe'); iframe.style.position = "absolute"; iframe.style.top = "0"; iframe.style.bottom = "0"; iframe.style.left = "0"; iframe.style.right = "0"; iframe.style.width = "100%"; iframe.style.height = "100%"; iframe.style.border = "none"; iframe.style.margin = "none"; iframe.style.zIndex = "999999"; iframe.src = src; win.document.body.appendChild(iframe)}, 100); 
 }
 
 function motox3m(){
     let url2 = "https://www.coolmathgames.com/0-moto-x3m/play";
-    var win = window.open('./plyr/')
-    var temp_url2 = btoa(url2);
+    let win = window.open('./plyr/')
+    let temp_url2 = btoa(url2);
     let src = "https://" + url + "/uv.html#" + temp_url2
-    setTimeout(function(){var iframe = win.document.createElement('iframe'); iframe.style.position = "absolute"; iframe.style.top = "0"; iframe.style.bottom = "0"; iframe.style.left = "0"; iframe.style.right = "0"; iframe.style.width = "100%"; iframe.style.height = "100%"; iframe.style.border = "none"; iframe.style.margin = "none"; iframe.style.zIndex = "999999"; iframe.src = src; win.document.body.appendChild(iframe)}, 100); 
+    setTimeout(function(){let iframe = win.document.createElement('iframe'); iframe.style.position = "absolute"; iframe.style.top = "0"; iframe.style.bottom = "0"; iframe.style.left = "0"; iframe.style.right = "0"; iframe.style.width = "100%"; iframe.style.height = "100%"; iframe.style.border = "none"; iframe.style.margin = "none"; iframe.style.zIndex = "999999"; iframe.src = src; win.document.body.appendChild(iframe)}, 100); 
 }
 
 
