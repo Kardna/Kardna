353c9fc2e5367a8b16d06b57a42e1119ac207c00
Zenfy <jackclancy470@gmail.com>
2023-03-30 16:56:53 -0400

    v4

--- v4/games/files/cloud/app.js
+++ v4/games/files/cloud/app.js
@@ -22,5 +22,20 @@ function motox3m(){
     setTimeout(function(){let iframe = win.document.createElement('iframe'); iframe.style.position = "absolute"; iframe.style.top = "0"; iframe.style.bottom = "0"; iframe.style.left = "0"; iframe.style.right = "0"; iframe.style.width = "100%"; iframe.style.height = "100%"; iframe.style.border = "none"; iframe.style.margin = "none"; iframe.style.zIndex = "999999"; iframe.src = src; win.document.body.appendChild(iframe)}, 100); 
 }
 
+function TinyFishing(){
+    let url2 = "https://www.coolmathgames.com/0-tiny-fishing/play";
+    let win = window.open('./plyr/')
+    let temp_url2 = btoa(url2);
+    let src = "https://" + url + "/uv.html#" + temp_url2
+    setTimeout(function(){let iframe = win.document.createElement('iframe'); iframe.style.position = "absolute"; iframe.style.top = "0"; iframe.style.bottom = "0"; iframe.style.left = "0"; iframe.style.right = "0"; iframe.style.width = "100%"; iframe.style.height = "100%"; iframe.style.border = "none"; iframe.style.margin = "none"; iframe.style.zIndex = "999999"; iframe.src = src; win.document.body.appendChild(iframe)}, 100); 
+}
+
+function Run3(){
+    let url2 = "https://www.coolmathgames.com/0-run-3/play";
+    let win = window.open('./plyr/')
+    let temp_url2 = btoa(url2);
+    let src = "https://" + url + "/uv.html#" + temp_url2
+    setTimeout(function(){let iframe = win.document.createElement('iframe'); iframe.style.position = "absolute"; iframe.style.top = "0"; iframe.style.bottom = "0"; iframe.style.left = "0"; iframe.style.right = "0"; iframe.style.width = "100%"; iframe.style.height = "100%"; iframe.style.border = "none"; iframe.style.margin = "none"; iframe.style.zIndex = "999999"; iframe.src = src; win.document.body.appendChild(iframe)}, 100); 
+}
 
 
